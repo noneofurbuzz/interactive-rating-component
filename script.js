3334adcd61e30fc67626269b6cc237299b4fe7d9
noneofurbuzz <ugochiike26@gmail.com>
2023-04-30 23:43:24 +0100

    Update with media queries

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 function number(value){
     localStorage.setItem("Value",value);
+    document.getElementById(value).style.backgroundColor = "hsl(217, 12%, 63%)";
+    document.getElementById(value).style.color = "hsl(0, 0%, 100%)";
+    
 }
 function message(){
+   if (localStorage.getItem("Value") == null){
+    alert("Please choose a rating");
+   }
+   else{
     location.replace("message.html");
+   }
 }
+function again(){
+    location.replace("index.html");
+    localStorage.clear();
+}
+
 
 
 

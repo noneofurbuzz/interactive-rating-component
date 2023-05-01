4dafe9229ed27e164d3cc3418f2936ca81e38361
noneofurbuzz <ugochiike26@gmail.com>
2023-05-01 02:09:37 +0100

    Edit style

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 function number(value){
+    const values = ["1","2","3","4","5"];
     localStorage.setItem("Value",value);
-    document.getElementById(value).style.backgroundColor = "hsl(217, 12%, 63%)";
+    document.getElementById(value).style.backgroundColor = "hsl(25, 97%, 53%)";
     document.getElementById(value).style.color = "hsl(0, 0%, 100%)";
-    
+    for(let i =0;i < values.length;i = i + 1){
+        if (value !== values[i]){
+            document.getElementById(values[i]).style.backgroundColor = "hsla(213, 19%, 21%,0.9)";
+            document.getElementById(values[i]).style.color = "hsl(217, 12%, 63%)";
+        }
+    }
 }
 function message(){
    if (localStorage.getItem("Value") == null){
@@ -14,7 +20,6 @@ function message(){
 }
 function again(){
     location.replace("index.html");
-    localStorage.clear();
 }
 
 
